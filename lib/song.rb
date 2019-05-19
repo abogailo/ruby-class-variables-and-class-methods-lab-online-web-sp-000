d9b01d37850981d05feada4af389e83e97bbd8e5
Abagail Hudson <abagailanne@gmail.com>
2019-05-19 22:23:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,48 +1,42 @@
 class Song
- attr_accessor = :name, :artist, :genre
- @@count = 0
- #this counts stuffs
- @@artists = []
- #this is a class variable used by class method artists
- @@genres = []
- #this is a class variable used by class method genres
+  attr_accessor :name, :artist, :genre
 
- def initialize(name, artist, genre)
+   @@count = 0
+  @@genres = []
+  @@artists = []
 
-   @name = name
-   @artist = artist
-   @genre = genre
+   def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
 
-   @@artists << artist
-   @@genres << genre
-   @@count += 1
+     @@artists << artist
+    @@genres << genre
+    @@count += 1
+  end
 
- end
+   def self.count
+    @@count
+  end
 
- def self.count
-  @@count
- end
+   def self.genres
+    @@genres.uniq  # returns a new array by removing duplicate values in self
+  end
 
- def self.artists
-   @@artists.uniq
-   #this only grabs an artist one time. No Beyonce, beyonce in this list
- end
+   def self.artists
+    @@artists.uniq # https://ruby-doc.org/core-2.2.0/Array.html#method-i-uniq
+  end
 
- def self.genres
-   @@genres.uniq
-   #this only grabs a genre once. There will be no duplicates.
- end
+   def self.genre_count
+    @@genres.each_with_object(Hash.new(0)) do |key, hash| #research for better understanding
+      hash[key] += 1
+    end
+  end
 
- def genre_count
-   @@genres.each_with_object(Hash.new(0)) do |key, hash|
-     hash[key] += 1
-   end
- end
-
- def artist_count
-   @@artists.each_with_object(Hash.new(0)) do |key, hash|
-   hash[key] += 1
- end
- end
+   def self.artist_count
+    @@artists.each_with_object(Hash.new(0)) do |key, hash|
+      hash[key] += 1
+    end
+  end
 
 end
