be5abb16f844623030b20ef91fc811eeb739e115
Abagail Hudson <abagailanne@gmail.com>
2019-05-19 22:25:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,9 @@
 class Song
- attr_accessor = :name, :artist, :genre
+ attr_accessor :name, :artist, :genre
+ 
  @@count = 0
- #this counts stuffs
  @@artists = []
- #this is a class variable used by class method artists
  @@genres = []
- #this is a class variable used by class method genres
 
  def initialize(name, artist, genre)
 
